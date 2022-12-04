3ef90da7d9a3c8f5ad9cc3057209b3cab41c9e75
Stimils02 <99268212+Stimils02@users.noreply.github.com>
2022-12-03 22:36:24 -0500

    misc

--- 348-proj/hometime/hometime/models.py
+++ 348-proj/hometime/hometime/models.py
@@ -20,17 +20,4 @@ class people(models.Model):
     def __str__(self):
         return self.dad
 
-"""class User2(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    userID = models.CharField(max_length=10)
-    email = models.CharField(max_length=50)
-    bio = models.CharField(max_length=300)
-    imagePath = models.CharField(max_length=300)
-
-    def __str__(self):
-        return self.firstname
-"""
-
-
 
